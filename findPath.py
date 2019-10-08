f1038a3b73de020a22819f0edb39e9ab641bea3f
azzyjk <azzyjk@naver.com>
2019-10-08 14:28:31 +0000

    Change appkey to invisible

---
+++ findPath.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import pprint
+import time
+
+readT = open('tmapKey.txt', mode='rt', encoding='utf-8')
+key = readT.read()
+
+url = 'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1'
+
+params = {'startX':'126.956167', 
+		   'startY':'37.390458',
+		  'speed':'300',
+		   'endX':'126.956971', 
+		   'endY':'37.393164',
+		   'startName':'%EC%B6%9C%EB%B0%9C',
+		   'endName':'%EB%B3%B8%EC%82%AC',
+		  'appKey':key
+		  }
+
+res = requests.post(url, data=params)
+
+for i in res.json()['features']:
+	pprint.PrettyPrinter(indent=4).pprint(i['geometry']['coordinates'][0])
+
+	#res.json['features'][0]['properties']
+	#pprint.PrettyPrinter(indent=4).pprint(res.json()['features'])
