ec7e0004a1b0766865e08f50559b0e2c9c8a6bd5
azzyjk <azzyjk@naver.com>
2019-10-09 05:53:26 +0000

    get lon & lat from Tmap

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import json
 import pprint
 import time
 
+def mkGP(lon, lat):
+	Gparams = {'locations':str(lat)+", "+str(lon),
+		  	   'key':keyG
+			  }
+	return Gparams
+
 #make udf to make gparams
 readT = open('tmapKey.txt', mode='rt', encoding='utf-8')
 keyT = readT.read()
@@ -10,11 +16,12 @@ keyT = readT.read()
 readG = open('gmapKey.txt', mode='rt', encoding='utf-8')
 keyG = readG.read()
 
-Turl = 'https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1'
+Turl = 'https://apis.openapi.sk.com/tmap/routes/pedestrian'
 Gurl = 'https://maps.googleapis.com/maps/api/elevation/json'
 
 #Tmap parameters
-Tparams = {'startX':'126.956167', 
+Tparams = {'version':'1',
+			'startX':'126.956167', 
 		   'startY':'37.390458',
 		  'speed':'300',
 		   'endX':'126.956971', 
@@ -24,18 +31,20 @@ Tparams = {'startX':'126.956167',
 		   'appKey':keyT
 		  }
 
-#Gmap parameters
-Gparams = {'locations':'37.390458, 126.956167',
-		  'key':keyG
-		  }
-
 #find the path
-Tres = requests.post(Turl, data=params)
+Tres = requests.post(Turl, data=Tparams)
+
+#find elevation
+Gparams = mkGP( 126.56167,37.390458)
+
+Gres = requests.get(Gurl, params=Gparams)
+
+#pprint.PrettyPrinter(indent=4).pprint(Gres.json())
+
+lists = []
 
 for i in Tres.json()['features']:
-	pprint.PrettyPrinter(indent=4).pprint(i['geometry']['coordinates'][0])
-	
-
-res = requests.get(Gurl, params=params)
-
-pprint.PrettyPrinter(indent=4).pprint(Gres.json())
+	if type(i['geometry']['coordinates'][0]) == float :
+		lists.append(i['geometry']['coordinates'])
+		
+print(type(str(lists[0])))
