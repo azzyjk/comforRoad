591865fedfb959b4fabdf5b03ebacd8a7211da1c
azzyjk <azzyjk@naver.com>
2019-10-09 13:48:40 +0000

    Add show Max & min elevation

--- pathNelevation.py
+++ pathNelevation.py
@@ -19,7 +19,8 @@ keyG = readG.read()
 Turl = 'https://apis.openapi.sk.com/tmap/routes/pedestrian'
 Gurl = 'https://maps.googleapis.com/maps/api/elevation/json'
 
-lists = []
+listPath = []
+listEle = []
 
 #Tmap parameters
 Tparams = {'version':'1',
@@ -38,12 +39,15 @@ Tres = requests.post(Turl, data=Tparams)
 
 for i in Tres.json()['features']:
 	if type(i['geometry']['coordinates'][0]) == float :
-		lists.append(i['geometry']['coordinates'])
+		listPath.append(i['geometry']['coordinates'])
 
 #find elevation
-for i in lists:
+for i in listPath:
 	Gparams = mkGP(i[0], i[1])
 
 	Gres = requests.get(Gurl, params=Gparams)
+	listEle.append(Gres.json()['results'][0]['elevation'])
 	pprint.PrettyPrinter(indent=4).pprint(Gres.json()['results'][0]['elevation'])
 	
+print("Max elevation :", max(listEle), "Min elevation :", min(listEle))
+
